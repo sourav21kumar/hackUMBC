4c9ab4f049c4ebfbf8a60385fb308026ba56c84a
sourav21kumar <sourav21kumar@gmail.com>
2021-10-26 21:28:50 +0530

    hackUMBC API

--- controller/route_controller.js
+++ controller/route_controller.js
@@ -1,14 +1,19 @@
 const User = require('../model/user')
 
-module.exports.api_post = async (req,res)=>{
+module.exports.api_post = async (req, res) => {
 
     const data = req.body;
+    try {
+        const savedUser = await User.create({
+            name: data.name,
+            temp: data.tempreture
+        })
 
-    const savedUser = await User.create({
-        name:data.name,
-        temp:data.tempreture
-    })
+        console.log(savedUser)
+        res.status(200).send({ status: 'OK', Info: savedUser })
+    } catch (err) {
+        console.log(err);
+        res.status(400).send(err);
+    }
 
-    console.log(savedUser)
-    res.status(200).send({status:'OK',Info : savedUser})
 }
